0a2ea63f8442584f49ca79d7a91d92771b2568bc
Vu Vo <vu.voth@gmail.com>
2021-11-05 02:43:18 +0700

    update relayer

--- packages/relayer/src/server.js
+++ packages/relayer/src/server.js
@@ -8,7 +8,8 @@ app.use(express.json());
 const config = require('./config');
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("access-control-allow-origin", "*");
     next();
 })
 
@@ -25,10 +26,11 @@ app.get("/", async (req, res) => {
 app.post("/zkproof/:sessionId", async (req, res) => {
     const { mTree } = req.body;
     const { sessionId } = req.params;
-    writeFile(__dirname + `/data/${process.env.CONTRACT_ADDR}/${sessionId}`, JSON.stringify(mTree), (err) => {
+    writeFile(__dirname + `/data/${process.env.CONTRACT_ADDR}_${sessionId}.json`, JSON.stringify({ mTree }, null, 2), (err) => {
         if (!err) {
             res.send({ success: true });
         } else {
+            console.log(err);
             res.send({ error: err });
         }
     });
@@ -36,11 +38,12 @@ app.post("/zkproof/:sessionId", async (req, res) => {
 
 // fetch merket tree
 app.get("/zkproof/:sessionId", async (req, res) => {
-    readFile(__dirname + `/data/${process.env.CONTRACT_ADDR}/${sessionId}`, (err, data) => {
+    const { sessionId } = req.params;
+    readFile(__dirname + `/data/${process.env.CONTRACT_ADDR}_${sessionId}`, (err, data) => {
         if (!err) {
             res.send(JSON.parse(data));
         } else {
-            res.send({error: err})
+            res.send({ error: err })
         }
     })
 })
@@ -65,7 +68,8 @@ app.get("/session/:sessionId", async (req, res) => {
             result
         })
     } catch (err) {
-        res.send({ err: err.message })
+        res.status(400);
+        res.send({ error: err })
     }
 })
 
